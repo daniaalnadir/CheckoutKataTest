d03c035ae1ae1db9d173f5ad0d736593cbadcbaa
Daniaal Nadir <daniaal1030@gmail.com>
2024-04-03 17:03:14 +0100

    Add initial tests

---
+++ CheckoutKataTest.Tests.Unit/Tests/CheckoutTests.cs
@@ -0,0 +1,58 @@
+﻿namespace CheckoutKataTest.Tests.Unit.Tests;
+
+public class CheckoutTests
+{
+    public CheckoutTests()
+    {
+        
+    }
+
+    [Fact]
+    public void GetTotalPrice_ShouldReturnCorrectRegularPrice_WhenNoStrategyPassedToCheckout()
+    {
+        
+    }
+
+    [Fact]
+    public void GetTotalPrice_ShouldReturnCorrectVolumePrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExact()
+    {
+    }
+
+    [Fact]
+    public void GetTotalPrice_ShouldReturnCorrectVolumePrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExceeded()
+    {
+    }
+
+    [Fact]
+    public void GetTotalPrice_ShouldFallbackToRegularStrategy_WhenVolumeStrategyPassedToCheckoutAndThresholdIsBelow()
+    {
+    }
+
+    [Fact]
+    public void
+        GetTotalPrice_ShouldStackDiscountAndReturnCorrectVolumePrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExceeded()
+    {
+    }
+
+    [Fact]
+    public void
+        GetTotalPrice_ShouldStackDiscountAndReturnCorrectPrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExceededAndMixProductsPassed()
+    {
+    }
+
+    [Fact]
+    public void
+        GetTotalPrice_ShouldReturnCorrectPrice_WhenMultipleProductsAndStrategiesPassedAndThresholdsMetExactly()
+    {
+    }
+
+    [Fact]
+    public void GetTotalPrice_ShouldReturnCorrectPrice_WhenMultipleProductsAndStrategiesPassedAndThresholdsExceeded()
+    {
+    }
+
+    [Fact]
+    public void GetTotalPrice_ShouldReturnCorrectPrice_WhenMultipleStrategiesPassedAndThresholdIsNotExceeded()
+    {
+    }
+}
