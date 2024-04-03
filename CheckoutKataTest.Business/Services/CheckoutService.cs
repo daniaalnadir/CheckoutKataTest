d26a62f2e8469ab2d8bcce76d1cc673dd6f87889
Daniaal Nadir <daniaal1030@gmail.com>
2024-04-03 17:20:03 +0100

    Add checkoutservice and pricing strategy classes

---
+++ CheckoutKataTest.Business/Services/CheckoutService.cs
@@ -0,0 +1,16 @@
+﻿using CheckoutKataTest.Business.Services.Contracts;
+
+namespace CheckoutKataTest.Business.Services;
+
+public class CheckoutService : ICheckoutService
+{
+    public void Scan(string item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
+}
