1baf6973692050a4f32a8a3fd68be99b15b1bbb8
Daniaal Nadir <daniaal1030@gmail.com>
2024-04-03 17:04:49 +0100

    Add initial folder structure

--- CheckoutKataTest.Tests.Unit/Tests/CheckoutTests.cs
+++ CheckoutKataTest.Tests.Unit/Tests/CheckoutTests.cs
@@ -10,49 +10,93 @@ public class CheckoutTests
     [Fact]
     public void GetTotalPrice_ShouldReturnCorrectRegularPrice_WhenNoStrategyPassedToCheckout()
     {
+        // Arrange
         
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void GetTotalPrice_ShouldReturnCorrectVolumePrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExact()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void GetTotalPrice_ShouldReturnCorrectVolumePrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExceeded()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void GetTotalPrice_ShouldFallbackToRegularStrategy_WhenVolumeStrategyPassedToCheckoutAndThresholdIsBelow()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void
         GetTotalPrice_ShouldStackDiscountAndReturnCorrectVolumePrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExceeded()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void
         GetTotalPrice_ShouldStackDiscountAndReturnCorrectPrice_WhenVolumeStrategyPassedToCheckoutAndThresholdIsExceededAndMixProductsPassed()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void
         GetTotalPrice_ShouldReturnCorrectPrice_WhenMultipleProductsAndStrategiesPassedAndThresholdsMetExactly()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void GetTotalPrice_ShouldReturnCorrectPrice_WhenMultipleProductsAndStrategiesPassedAndThresholdsExceeded()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 
     [Fact]
     public void GetTotalPrice_ShouldReturnCorrectPrice_WhenMultipleStrategiesPassedAndThresholdIsNotExceeded()
     {
+        // Arrange
+        
+        // Act
+        
+        // Assert
     }
 }
